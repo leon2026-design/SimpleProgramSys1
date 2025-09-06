ebcb5d03cbfc2e1aee409e572b792c266362355c
leon2026-design <Gonzales.260@osu.edu>
2025-09-05 23:28:53 -0400

    Update simpleCProgram.c

--- simpleCProgram.c
+++ simpleCProgram.c
@@ -1,1 +1,15 @@
-#include <stdio.h>\n\nint main() {\n    printf("Hello, World!\n");\n    return 0;\n}
+#include <stdio.h>
+
+int main() {
+    int numbers[] = {1, 2, 3, 4, 5}; 
+    int length = sizeof(numbers) / sizeof(numbers[0]); 
+    int sum = 0;
+
+    for (int i = 0; i < length; i++) {
+        sum += numbers[i];
+    }
+
+    printf("The sum of the array is: %d\n", sum);
+
+    return 0;
+}
